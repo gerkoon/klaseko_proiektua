88047f8742b9c8a5b3febf7d2e5baf71272990b6
gerkoon <gerkoon@gmail.com>
2013-01-28 14:40:54 +0100

    Update lib/model/entities/zentrua.php

--- lib/model/entities/zentrua.php
+++ lib/model/entities/zentrua.php
@@ -4,13 +4,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+namespace entities;
 /**
  * Description of zentrua
  *
  * @author ikasle
  */
-/** @entity*/
+/** @Entity  @Table(name="zentrua") */
 class zentrua {
     /** @Id @GeneratedValue(strategy="AUTO") @Column(type="integer") **/
     private $id;
@@ -33,6 +33,21 @@ class zentrua {
     
     /** @Column(type="boolean")**/
     private $ordainduta;
+
+    
+    public function __construct($desk,$id_b,$eguna){
+        $this->deskribapena=$desk;
+        $this->id_bezero=$id_b;
+        $this->eguna=$eguna;
+        $this->ordainduta=false;
+        
+    }
+    
+    public function setPrezioa($p) {
+        $this->prezioa=$p;
+    }
 }
+
+    
 
 ?>
