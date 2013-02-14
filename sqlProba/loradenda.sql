eb7c6020e57feccafcb8f42e19a4431e4ba5c68e
ikasle <ikasle@zornotza-119dce>
2013-02-14 12:35:28 +0100

    aplikazioa 1.2 bukatuta

--- sqlProba/loradenda.sql
+++ sqlProba/loradenda.sql
@@ -34,28 +34,7 @@ CREATE TABLE IF NOT EXISTS `bezeroa` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=217 ;
 
---
--- Volcado de datos para la tabla `bezeroa`
---
 
-INSERT INTO `bezeroa` (`id`, `izena`, `eguna`, `ordainduta`) VALUES
-(191, 'bezero1', 30, 0),
-(192, 'bezero2', 31, 0),
-(193, 'bezero3', 31, 0),
-(194, 'bezero4', 31, 0),
-(200, 'bezero00', 31, 0),
-(201, 'bezero01', 1, 0),
-(206, 'bbezero', 1, 0),
-(207, 'bat', 1, 0),
-(208, 'asd', 1, 0),
-(209, 'a ver', 1, 1),
-(210, 'vavavava', 1, 0),
-(211, 'mikel', 1, 1),
-(212, 'azken proba', 1, 1),
-(213, 'fddsdfds', 1, 0),
-(214, 'a', 1, 1),
-(215, 'dssds', 1, 0),
-(216, 'mikel esteban2', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -69,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `eguna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Volcado de datos para la tabla `eguna`
+-- Volcado de datos para la tabla `eguna` beste batzuk badira hemen jarri behar dira
 --
 
 INSERT INTO `eguna` (`eguna`) VALUES
@@ -91,27 +70,6 @@ CREATE TABLE IF NOT EXISTS `zentrua` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=516 ;
 
---
--- Volcado de datos para la tabla `zentrua`
---
-
-INSERT INTO `zentrua` (`id`, `deskribapena`, `prezioa`, `id_bezero`) VALUES
-(500, 'centro variado', 10, 191),
-(501, '1/2 clavel rojo', 11, 191),
-(502, '1doz larrosa gorri', 13, 192),
-(503, 'ramito clavel', 5.09, 193),
-(504, 'centro clavel blanco', 0, 194),
-(505, 'centro variado', 0, 200),
-(506, '1 rosa', 0, 200),
-(507, 'centro margaritas', 0, 201),
-(508, 'fdafafsafa', 0, 210),
-(509, 'zentru1', 0, 211),
-(510, 'zentru2', 0, 211),
-(511, 'zentru3', 0, 211),
-(512, 'fdsfdsfds', 0, 212),
-(513, 'fdsfdsds', 0, 213),
-(514, 'dsadsadsa', 0, 214),
-(515, 'zentru espezifikatu gabea', 0, 216);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
